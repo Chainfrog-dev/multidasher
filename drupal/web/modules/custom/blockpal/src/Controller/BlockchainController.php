76a6d5ba677ad7ac837f05d30e417b798502342f
edward@chainfrog.com <edward@chainfrog.com>
2018-10-08 18:19:20 +0200

    controller

--- drupal/web/modules/custom/blockpal/src/Controller/BlockchainController.php
+++ drupal/web/modules/custom/blockpal/src/Controller/BlockchainController.php
@@ -31,6 +31,7 @@ class BlockchainController extends ControllerBase {
   public function connectMultichainIp(String $port, String $ip, String $name) {
     system('multichaind '.$name.'@'.$ip.':'.$port.' -datadir="/var/www/.multichain" > /var/www/.multichain/'.$name.'/startup.dat 2>&1 &', $status);
     drupal_set_message('multichaind '.$name.'@'.$ip.':'.$port.' -datadir="/var/www/.multichain" > /var/www/.multichain/'.$name.'/startup.dat 2>&1 &');
+    drupal_set_message($status);
     return $status;
     // multichaind edtest@207.154.216.254:2893 -datadir="/var/www/.multichain" > ./debug.log 2>&1 & 
   }

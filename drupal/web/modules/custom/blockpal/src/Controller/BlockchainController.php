56db8a7fd0ed20f889198d5e23859d79a0eb39c4
edward@chainfrog.com <edward@chainfrog.com>
2018-10-08 17:37:40 +0200

    connect script

--- drupal/web/modules/custom/blockpal/src/Controller/BlockchainController.php
+++ drupal/web/modules/custom/blockpal/src/Controller/BlockchainController.php
@@ -366,8 +366,8 @@ class BlockchainController extends ControllerBase {
 
   public function retrieveWalletAddress(String $blockchain) {
     drupal_set_message('retrieveWalletAddress');
-    $text = cat('/var/www/.multichain/edtest/startup.dat');
-    ksm($test);
+    $text = shell_exec('cat /var/www/.multichain/edtest/startup.dat');
+    ksm($text);
     $file = '/var/www/.multichain/' . $blockchain . '/startup.dat';
 
     drupal_set_message($file);

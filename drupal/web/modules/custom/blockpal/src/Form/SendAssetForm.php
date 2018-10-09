34efc6398ff8cba0a8f48ba6c2ef995a8e53834b
edward@chainfrog.com <edward@chainfrog.com>
2018-10-09 23:27:33 +0200

    cleaning and changing starting db

--- drupal/web/modules/custom/blockpal/src/Form/SendAssetForm.php
+++ drupal/web/modules/custom/blockpal/src/Form/SendAssetForm.php
@@ -10,6 +10,9 @@ use Drupal\node\Entity\Node;
 use Drupal\blockpal\Controller\ManageRequestsController;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\InvokeCommand;
+use Drupal\views\Views;
+use Symfony\Component\HttpFoundation\Request;
+
 
 /**
  * Class SendAssetForm.
@@ -35,12 +38,40 @@ class SendAssetForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['text_area'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'ajax-wrapper'],
+      '#prefix' => '<h2>Address Book</h2>',
+    ];
+
     // Default settings.
+    $view = Views::getView('recipients');
+    if (is_object($view)) {
+        $view->setArguments(['1334']);
+        $view->setDisplay('default');
+        $view->preExecute();
+        $view->execute();
+        $result = $view->result;
+        if($result){
+        foreach ($result as $key => $value) {
+          $node = Node::load(($value->nid));
+          $title = $node->label();
+          $address = $node->field_recipient_wallet_address->getString();
+          $asset = Node::load($node->field_recipient_asset->getString());
+          $asset_title = $asset->label();
+          $form['text_area'][$title]['#markup'] = '<div class="row">Title: '.$title.'</div><div class="row">Address: '.$address.'</div><div class="row">Asset: '.$asset_title.'</div>';
+          $form['text_area'][$title]['#prefix'] = '<h3>'.$title.'</h3>';
+          $form['text_area'][$title]['#suffix'] = '</br>';
+        }
+      }
+    }
 
     $form['ajax_wrapper'] = [
       '#type' => 'container',
       '#attributes' => ['id' => 'ajax-wrapper'],
     ];
+
 
     $blockchains = $this->loadBlockchainOptions();
     $options = array_flip($blockchains);

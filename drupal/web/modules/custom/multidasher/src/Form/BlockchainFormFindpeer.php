181de6c5ca759120be447089dc539b08b7bbd0f4
edward@chainfrog.com <edward@chainfrog.com>
2018-10-10 09:42:38 +0200

    connect to peer

--- drupal/web/modules/custom/multidasher/src/Form/BlockchainFormFindpeer.php
+++ drupal/web/modules/custom/multidasher/src/Form/BlockchainFormFindpeer.php
@@ -7,7 +7,6 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
 use Drupal\multidasher\Controller\BlockchainControllerFindpeer;
-use Drupal\multidasher\Controller\BlockchainControllerFindpeer;
 use Drupal\multidasher\Controller\ReadStdoutController;
 use Drupal\Core\Block\BlockPluginInterface;
 

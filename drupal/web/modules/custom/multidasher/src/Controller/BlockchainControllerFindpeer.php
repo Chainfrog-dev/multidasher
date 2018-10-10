8bc2b6cddee1dd39d5fda330d707a47b0dc330f6
edward@chainfrog.com <edward@chainfrog.com>
2018-10-10 11:49:57 +0200

    read stdout

--- drupal/web/modules/custom/multidasher/src/Controller/BlockchainControllerFindpeer.php
+++ drupal/web/modules/custom/multidasher/src/Controller/BlockchainControllerFindpeer.php
@@ -45,34 +45,34 @@ class BlockchainControllerFindpeer extends ControllerBase {
     //     "whitelisted" : false
     // }');
 
-    // foreach ($result as $key => $value) {
+    foreach ($result as $key => $value) {
 
-    //   $nodes = \Drupal::entityTypeManager()
-    //     ->getStorage('node')
-    //     ->loadByProperties(['field_peer_address' => $value->addr]);
+      $nodes = \Drupal::entityTypeManager()
+        ->getStorage('node')
+        ->loadByProperties(['field_peer_address' => $value->addr]);
 
-    //   if ($node = reset($nodes)) {
-    //     $node->set('field_peer_address', $value->addr);
-    //     $node->set('field_peer_address_local', $value->addrlocal);
-    //     $node->set('field_peer_id', $value->id);
-    //     $node->field_peer_blockchain_ref = ['target_id' => $blockchain_nid];
-    //   }
-    //   else {
+      if ($node = reset($nodes)) {
+        $node->set('field_peer_address', $value->addr);
+        $node->set('field_peer_address_local', $value->addrlocal);
+        $node->set('field_peer_id', $value->id);
+        $node->field_peer_blockchain_ref = ['target_id' => $blockchain_nid];
+      }
+      else {
 
-    //     $node = Node::create(['type' => 'blockchain_peer']);
-    //     $node->set('title', $value->id);
-    //     $node->set('field_peer_address', $value->addr);
-    //     $node->set('field_peer_address_local', $value->addrlocal);
-    //     $node->set('field_peer_id', $value->id);
-    //     $node->field_peer_blockchain_ref = ['target_id' => $blockchain_nid];
-    //     $node->status = 1;
-    //     $node->enforceIsNew();
+        $node = Node::create(['type' => 'blockchain_peer']);
+        $node->set('title', $value->id);
+        $node->set('field_peer_address', $value->addr);
+        $node->set('field_peer_address_local', $value->addrlocal);
+        $node->set('field_peer_id', $value->id);
+        $node->field_peer_blockchain_ref = ['target_id' => $blockchain_nid];
+        $node->status = 1;
+        $node->enforceIsNew();
 
-    //   }
+      }
 
-    //   $node->save();
+      $node->save();
 
-    // }
+    }
     return new RedirectResponse(base_path() . 'multidasher');
   }
 
